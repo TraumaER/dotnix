1f46460f23253b5e8d894d87a37191adc0022e22
Adam Bannach <113929542+abannachGrafana@users.noreply.github.com>
2025-09-17 14:58:42 -0500

    feat: add posting; git aliases; fzf shell integration

--- home/shared.nix
+++ home/shared.nix
@@ -18,6 +18,7 @@
     alejandra # Nix formatter
     bat
     curl
+    posting
 
     # Docker stuff
     docker
@@ -92,6 +93,19 @@
           };
         };
       };
+      aliases = {
+        # https://fortes.com/2022/make-git-better-with-fzf/
+        addm = "!git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git add";
+        addmp = "!git ls-files --deleted --modified --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r -o git add -p";
+        cb = "!git branch --all | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --preview 'git show --color=always {-1}' | sed 's/remotes\\/origin\\///g' | xargs -r git checkout";
+        cs = "!git stash list | fzf -0 --preview 'git show --pretty=oneline --color=always --patch \"$(echo {} | cut -d: -f1)\"' | cut -d: -f1 | xargs -r git stash pop";
+        db = "!git branch | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --multi --preview 'git show --color=always {-1}' | xargs -r git branch --delete";
+        Db = "!git branch | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --multi --preview 'git show --color=always {-1}' | xargs -r git branch --delete --force";
+        ds = "!git stash list | fzf -0 --preview 'git show --pretty=oneline --color=always --patch \"$(echo {} | cut -d: -f1)\"' | cut -d: -f1 | xargs -r git stash drop";
+        edit = "!git ls-files --modified --other --exclude-standard | sort -u | fzf -0 --multi --preview 'git diff --color {}' | xargs -r $EDITOR -p";
+        fixup = "!git log --oneline --no-decorate --no-merges | fzf -0 --preview 'git show --color=always --format=oneline {1}' | awk '{print $1}' | xargs -r git commit --fixup";
+        resetm = "!git diff --name-only --cached | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git reset";
+      };
     };
 
     bash = {
@@ -146,9 +160,9 @@
     };
 
     fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        enableBashIntegration = true;
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
     };
 
     pay-respects = {
