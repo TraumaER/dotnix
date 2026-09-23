de68109b63bc676818eea874c906e46ee552dc2b
Adam Bannach <4845159+TraumaER@users.noreply.github.com>
2026-09-23 13:09:58 -0500

    fix(git): resolve Git LFS through PATH

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,6 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   imports = [./shared.nix];
-  programs.git.lfs.enable = true;
+
+  programs.git.lfs = {
+    enable = true;
+    package = null;
+  };
+
+  home.packages = [pkgs.git-lfs];
+
   programs.zsh.initContent = ''
     bindkey "^[[3~" delete-char
   '';
